3102b01dd3dce14243e4a78cf0d8b217dee53231
Kame <ev60537@meiji.ac.jp>
2019-06-18 11:12:00 +0900

    touchend, preventdefault

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
 let preparedText = "give when open without quickly long most car would away mean need mile few saw until say picture soon once because";
 
-let target = document.getElementById("target")
-target.addEventListener("click", (event) => {
-    console.log(true);
-    let [x, y] = [event.pageX, event.pageY]
+function inputKey(ev, x, y) {
     console.log(`x:${x}, y:${y}`)
     document.getElementById("x").innerHTML = x;
     document.getElementById("y").innerHTML = y;
     document.getElementById("input_text").innerHTML += preparedText[0]===" " ? "&nbsp;" : preparedText[0];
     preparedText = preparedText.slice(1);
+}
+
+let target = document.getElementById("target")
+target.addEventListener("touchend", (ev) => {
+    ev.preventDefault();
+    console.log(true);
+    let [x, y] = [ev.changedTouches[0].pageX, ev.changedTouches[0].pageY]
+    inputKey(ev, x, y);
+})
+
+target.addEventListener("click", (ev) => {
+    console.log(true);
+    let [x, y] = [ev.pageX, ev.pageY]
+    inputKey(ev, x, y);
 })
