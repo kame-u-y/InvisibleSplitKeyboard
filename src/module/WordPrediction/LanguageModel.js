8bad2048401cee3e88f2238e501254a64a965022
Kame <ev60537@meiji.ac.jp>
2019-10-18 21:02:26 +0900

    まるめこみが...

--- src/module/WordPrediction/LanguageModel.js
+++ src/module/WordPrediction/LanguageModel.js
@@ -1,11 +1,13 @@
 import { freqJson } from './wordFreqJson.js';
 
 export function getLMProbability(str) {
-  const rambda = 0.9995;
+  const rambda = 0.9999999995;
   let res = freqJson[str[0]].find(v => v.word === str);
   if (res) {
     res = Number(res.ratio) * rambda;
   } else {
+    console.log(Number(freqJson.total));
+    console.log(1 - rambda);
     res = {
       word: str,
       ratio: (1 - rambda) * (1 / Number(freqJson.total))
