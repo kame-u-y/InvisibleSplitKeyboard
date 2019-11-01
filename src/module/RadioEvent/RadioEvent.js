7d6524b669ea4acf6ce19d801daaf9cdf92b9232
Turtar <ffmstortoise62002@gmail.com>
2019-11-02 03:40:46 +0900

    eyesonとperipheralのモードわけ

--- src/module/RadioEvent/RadioEvent.js
+++ src/module/RadioEvent/RadioEvent.js
@@ -1,80 +1,81 @@
 // visual-modeの設定
-let leftKeyboard = $('#left-keyboard');
-let rightKeyboard = $('#right-keyboard');
-let keys = $('.key');
+let leftKeyboard = $("#left-keyboard");
+let rightKeyboard = $("#right-keyboard");
+let keys = $(".key");
 let unusedKeys = $(
-  '.shift, .left-symbol, .language, .mic, .back, .enter, .period, .comma, .close, .right-symbol'
+  ".shift, .left-symbol, .language, .mic, .back, .enter, .period, .comma, .close, .right-symbol"
 );
-let leftSpace = $('.left-space');
-let rightSpace = $('.right-space');
-let spaceVisible = $('#space-visible');
+let leftSpace = $(".left-space");
+let rightSpace = $(".right-space");
+let spaceVisible = $("#space-visible");
 
 function setKeyboardBGColor(isVisible) {
   if (isVisible) {
-    leftKeyboard.css('background-color', 'rgb(48, 48, 48)');
-    rightKeyboard.css('background-color', 'rgb(48, 48, 48)');
+    leftKeyboard.css("background-color", "rgb(48, 48, 48)");
+    rightKeyboard.css("background-color", "rgb(48, 48, 48)");
   } else {
-    leftKeyboard.css('background-color', 'rgba(48, 48, 48, 0)');
-    rightKeyboard.css('background-color', 'rgba(48, 48, 48, 0)');
+    leftKeyboard.css("background-color", "rgba(48, 48, 48, 0)");
+    rightKeyboard.css("background-color", "rgba(48, 48, 48, 0)");
   }
 }
 
 function setKeyboardBorderColor(isVisible) {
   if (isVisible) {
-    leftKeyboard.css('border-color', 'rgb(48, 48, 48)');
-    rightKeyboard.css('border-color', 'rgb(48, 48, 48)');
+    leftKeyboard.css("border-color", "rgb(48, 48, 48)");
+    rightKeyboard.css("border-color", "rgb(48, 48, 48)");
   } else {
-    leftKeyboard.css('border-color', 'rgba(48, 48, 48, 0)');
-    rightKeyboard.css('border-color', 'rgba(48, 48, 48, 0)');
+    leftKeyboard.css("border-color", "rgba(48, 48, 48, 0)");
+    rightKeyboard.css("border-color", "rgba(48, 48, 48, 0)");
   }
 }
 
 function setSpaceOpacity(isVisible) {
   if (isVisible) {
-    leftSpace.css('opacity', '1');
-    rightSpace.css('opacity', '1');
+    leftSpace.css("opacity", "1");
+    rightSpace.css("opacity", "1");
   } else {
-    leftSpace.css('opacity', '0');
-    rightSpace.css('opacity', '0');
+    leftSpace.css("opacity", "0");
+    rightSpace.css("opacity", "0");
   }
 }
 
 function setKeyOpacity(isVisible) {
   if (isVisible) {
-    keys.css('opacity', '1');
+    keys.css("opacity", "1");
     setSpaceOpacity(true);
   } else {
-    keys.css('opacity', '0');
-    if (spaceVisible.prop('checked')) {
+    keys.css("opacity", "0");
+    if (spaceVisible.prop("checked")) {
       setSpaceOpacity(true);
     } else {
       setSpaceOpacity(false);
     }
   }
-  unusedKeys.css('opacity', '0');
+  unusedKeys.css("opacity", "0");
 }
 
 function addRadioEvent() {
-  $('#visual-mode input:radio[name=visual-mode]').on('change', ev => {
-    let keys = $('.key');
+  $("#visual-mode input:radio[name=visual-mode]").on("change", ev => {
+    let keys = $(".key");
 
     switch (ev.target.value) {
-      case 'visible':
+      case "eyes-on":
+      case "peripheral":
         setKeyboardBGColor(true);
         setKeyboardBorderColor(true);
         setKeyOpacity(true);
         break;
-      case 'key-invisible':
+      case "key-invisible":
         setKeyboardBGColor(true);
         setKeyboardBorderColor(true);
         setKeyOpacity(false);
         break;
-      case 'frame-only':
+      case "frame-only":
         setKeyboardBGColor(false);
         setKeyboardBorderColor(true);
         setKeyOpacity(false);
         break;
-      case 'invisible':
+      case "invisible":
         setKeyboardBGColor(false);
         setKeyboardBorderColor(false);
         setKeyOpacity(false);
@@ -84,12 +85,11 @@ function addRadioEvent() {
 }
 
 function addSpaceCheckEvent() {
-  $('#space-visible').on('change', ev => {
-    if (
-      $('#visual-mode input:radio[name=visual-mode]:checked').val() ===
-      'visible'
-    )
-      return;
+  $("#space-visible").on("change", ev => {
+    const visualValue = $(
+      "#visual-mode input:radio[name=visual-mode]:checked"
+    ).val();
+    if (visualValue === "eyes-on" || visualValue === "peripheral") return;
     if (ev.target.checked) {
       setSpaceOpacity(true);
     } else {
