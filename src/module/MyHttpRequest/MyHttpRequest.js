143e3f046983d475ded257512244dfaafc2028fb
kame-v-d <ffmstortoise62002@gmail.com>
2021-02-12 03:07:23 +0900

    postTapDataを実装中

--- src/module/MyHttpRequest/MyHttpRequest.js
+++ src/module/MyHttpRequest/MyHttpRequest.js
@@ -1,71 +1,28 @@
 import * as kl from '../KeyList/KeyList.js';
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: 'AIzaSyBGPQ2pZFbRAMVDnMyC64jr1WGU-nFpt70',
-  authDomain: 'invisiblesplitkeyboard.firebaseapp.com',
-  databaseURL: 'https://invisiblesplitkeyboard.firebaseio.com',
-  projectId: 'invisiblesplitkeyboard',
-  storageBucket: 'invisiblesplitkeyboard.appspot.com',
-  messagingSenderId: '567801235303',
-  appId: '1:567801235303:web:c544d50e5d15b127b011c2',
-  measurementId: 'G-BBG6MG7CMW',
-};
-// Initialize Firebase
-export function initFirebase() {
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
-//a duck quakes to ask for food
-/*
-40, 187
-91, 189
-142, 189
-195, 188
-246, 187
-64, 252
-115, 252
-167, 250
-219, 252
-
-*/
-// x : 51
-// y : 188, 252
-
 export function postTapData(tapData) {
-  let a = [];
-  Object.keys(tapData).filter((v) => {
-    a = a.concat(tapData[v]);
-  });
-  a.sort((b, c) => (b.timestamp > c.timestamp ? 1 : -1));
-  console.log(a);
+  const data = {
+    user: $('#user-name').val(),
+    keyboardType: $('#visual-mode input:radio[name=visual-mode]:checked').val(),
+    spaceVisual:
+      $('#space-visible').prop('checked') | (keyboardType === 'visible')
+        ? 'visible'
+        : 'invisible',
+    tapData: tapData,
+  };
 
-  Object.keys(tapData).filter((v) => {
-    tapData[v] = firebase.firestore.FieldValue.arrayUnion(...tapData[v]);
-  });
-  const user = $('#user-name').val();
-  const keyboardType = $(
-    '#visual-mode input:radio[name=visual-mode]:checked'
-  ).val();
-  const spaceVisual =
-    $('#space-visible').prop('checked') | (keyboardType === 'visible')
-      ? 'visible'
-      : 'invisible';
-  let db = firebase.firestore();
-  db.collection('users')
-    .doc(user)
-    .collection('devices')
-    .doc('ipad9.7')
-    .collection('keyboardTypes')
-    .doc(keyboardType)
-    .collection('spaceVisual')
-    .doc(spaceVisual)
-    .set(tapData, { merge: true })
-    .then((docRef) => {
-      console.log(docRef);
-    })
-    .catch((error) => {
-      console.log('Error adding document: ', error);
+  const params = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch('https://invisiblesplitkeyboard.an.r.appspot.com/postTapData', params)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
     });
 }
 
