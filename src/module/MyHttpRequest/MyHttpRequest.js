b79a8383057cbb6e653269f5b25055a2468ef032
kame-v-d <ffmstortoise62002@gmail.com>
2021-02-12 05:46:52 +0900

    postTaskData多分いける

--- src/module/MyHttpRequest/MyHttpRequest.js
+++ src/module/MyHttpRequest/MyHttpRequest.js
@@ -111,96 +111,37 @@ export function getTapData(user, keyboardType, spaceVisual, callback) {
     .catch((error) => {
       console.error('Error:', error);
     });
-
-  // db.collection('users')
-  //   .doc(user)
-  //   .collection('devices')
-  //   .doc('ipad9.7')
-  //   .collection('keyboardTypes')
-  //   .doc(keyboardType)
-  //   .collection('spaceVisual')
-  //   .doc(spaceVisual)
-  //   .get()
-  //   .then((doc) => {
-  //     if (doc.exists) {
-  //       let data = Object.assign({}, doc.data());
-  //       Object.keys(data)
-  //         .filter((k) => !kl.keyList.find((v) => v === k))
-  //         .filter((k) => {
-  //           delete data[k];
-  //         });
-  //       // callback(data);
-  //       userData = data;
-  //       // isSetData.user = true;
-  //       // if (isSetData.default && isSetData.user) {
-  //       Object.keys(userData).filter((k) => {
-  //         // それぞれのキーに対してx座標が反対側のキーボードにあるやつを排除
-  //         let leftKey = 'qwertasdfgzxcv';
-  //         userData[k] = userData[k].filter((v) => {
-  //           return (
-  //             (v.position.x - window.outerWidth / 2.0) *
-  //               (leftKey.match(k) ? -1 : 1) >
-  //             0
-  //           );
-  //         });
-  //         // console.log(userData);
-  //         // userData[k] = userData[k].concat(defaultData[k]);
-  //       });
-  //       Object.keys(defaultData).map((k) => {
-  //         if (!userData[k]) {
-  //           userData[k] = [];
-  //         }
-  //         userData[k] = defaultData[k].concat(userData[k]);
-  //         return;
-  //       });
-  //       callback(userData);
-  //       // }
-  //     } else {
-  //       console.log('no such document');
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.log('error getting document:', error);
-  //   });
 }
 
 export function postTaskData(taskData) {
-  // let a = [];
-  // Object.keys(taskData).filter(v => {
-  //   a = a.concat(taskData[v]);
-  // });
-  // a.sort((b, c) => (b.timestamp > c.timestamp ? 1 : -1));
-  // console.log(a);
-
-  // Object.keys(taskData).filter(v => {
-  //   taskData[v] = firebase.firestore.FieldValue.arrayUnion(...taskData[v]);
-  // });
-  // taskData ;
-  const postData = {
-    taskData: firebase.firestore.FieldValue.arrayUnion(taskData),
-  };
-  const user = $('#user-name').val();
   const keyboardType = $(
     '#visual-mode input:radio[name=visual-mode]:checked'
   ).val();
-  const spaceVisual =
-    $('#space-visible').prop('checked') | (keyboardType === 'visible')
-      ? 'visible'
-      : 'invisible';
-  let db = firebase.firestore();
-  db.collection('users')
-    .doc(user + '-taskData')
-    .collection('devices')
-    .doc('ipad9.7')
-    .collection('keyboardTypes')
-    .doc(keyboardType)
-    .collection('spaceVisual')
-    .doc(spaceVisual)
-    .set(postData, { merge: true })
-    .then((docRef) => {
-      console.log(docRef);
+  const data = {
+    user: $('#user-name').val(),
+    keyboardType: keyboardType,
+    spaceVisual:
+      $('#space-visible').prop('checked') | (keyboardType === 'visible')
+        ? 'visible'
+        : 'invisible',
+    taskData: taskData,
+  };
+
+  const params = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    mode: 'cors',
+    body: JSON.stringify(data),
+  };
+
+  fetch('https://invisiblesplitkeyboard.an.r.appspot.com/postTaskData', params)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
     })
     .catch((error) => {
-      console.log('Error adding document: ', error);
+      console.error('Error:', error);
     });
 }
